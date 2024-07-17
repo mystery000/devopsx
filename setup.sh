c5d244c31ef7264db88add3ba6aa82b40b339eac
mystery000 <lkostyrka17@gmail.com>
2024-07-17 01:12:14 -0400

    Fix commit

--- setup.sh
+++ setup.sh
@@ -7,16 +7,15 @@ echo "Current Python version: $current_python_version"
 if [ "$(printf '%s\n' "$required_python_version" "$current_python_version" | sort -V | head -n1)" != "$required_python_version" ]; then
     echo "Installing Python $required_python_version..."
     sudo apt-get update
-    sudo apt-get install -y python3.10 python3.10-venv
+    sudo apt-get install -y python3.10
     sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
     echo "Python $required_python_version installed successfully."
 fi
 
-# Ensure the python3-venv package is installed
-if ! dpkg -l | grep -q python3-venv; then
-    echo "Installing python3-venv..."
-    sudo apt-get install -y python3-venv
-    echo "python3-venv installed successfully."
+if ! dpkg -l | grep -q python3.10-venv; then
+    echo "Installing python3.10-venv..."
+    sudo apt-get install -y python3.10-venv
+    echo "python3.10-venv installed successfully."
 fi
 
 # Activate virtual environment
