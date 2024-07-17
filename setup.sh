f2ab5d611e42757a8f3b74c46f0bf0ccdb7b07d8
mystery000 <lkostyrka17@gmail.com>
2024-07-17 00:07:33 -0400

    Hot Fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,51 +1,36 @@
 #!/bin/bash
 
-# Function to display loading messages
-function show_loading() {
-    local message=$1
-    echo -n "$message"
-    while :; do
-        echo -n "."
-        sleep 1
-    done
-}
+set -e  # Exit immediately if a command exits with a non-zero status
 
 # Check Python version and install if needed
-echo "Checking Python version..."
 current_python_version=$(python3 --version 2>&1 | cut -d' ' -f2)
 required_python_version="3.10"
-echo "Current Python version: $current_python_version"
+echo "Installed Python version: $current_python_version"
 
 if [ "$(printf '%s\n' "$required_python_version" "$current_python_version" | sort -V | head -n1)" != "$required_python_version" ]; then
     echo "Installing Python $required_python_version..."
     sudo apt-get update
     sudo apt-get install -y python3.10
     sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-    echo "Python $required_python_version installed successfully."
-else
-    echo "Python version is Okay"
+fi
+
+# Ensure the python3-venv package is installed
+if ! dpkg -l | grep -q python3.10-venv; then
+    echo "Installing python3.10-venv..."
+    sudo apt-get install -y python3.10-venv
+    echo "python3.10-venv installed successfully."
 fi
 
 # Activate virtual environment
 if [ -z "$VIRTUAL_ENV" ]; then
-    echo "Activating virtual environment..."
     python3 -m venv .venv
     source .venv/bin/activate
-    echo "Virtual environment activated."
 fi
 
-# Install requirements
-echo "Installing requirements..."
-#show_loading "Please wait"
 pip install poetry
 poetry install --extras "datascience"
-echo "Requirements installed successfully."
-
-# Clean up loading message
-kill $!
 
 # Deactivate virtual environment
 deactivate
-echo "Virtual environment deactivated."
 
 echo "Installation complete!"
