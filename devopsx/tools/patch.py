ad1c777bcd6fad63ca5a5c3c90853dcc29e8c2b4
Mohamed.H <mhafeel@computer.com>
2024-10-02 11:57:16 +0000

    feat: support placeholders in patches

--- devopsx/tools/patch.py
+++ devopsx/tools/patch.py
@@ -1,64 +1,84 @@
 """
-Gives the LLM agent the ability to patch files, by using a adapted version git conflict markers.
-
-Example:
-
-.. chat::
-
-    User: patch the file `hello.py` to ask for the name of the user
-    Assistant:
-    ```patch hello.py
-    <<<<<<< ORIGINAL
-    print("Hello world")
-    =======
-    name = input("What is your name? ")
-    print(f"hello {name}")
-    >>>>>>> UPDATED
-    ```
-    System: Patch applied
-
-Inspired by aider.
+Gives the LLM agent the ability to patch text files, by using a adapted version git conflict markers.
 """
 
 # TODO: support multiple patches in one codeblock (or make it clear that only one patch per codeblock is supported/applied)
 import re
-from collections.abc import Generator
 from pathlib import Path
+from typing import Literal
+from collections.abc import Generator
 
-from .base import ToolSpec
 from ..message import Message
 from ..util import ask_execute
+from .base import ToolSpec
 
 instructions = """
 To patch/modify files, we can use an adapted version of git conflict markers.
 
-This can be used to make changes to files we have written in the past, without having to rewrite the whole file.
+This can be used to make changes to files, without having to rewrite the whole file.
 Only one patch block can be written per codeblock. Extra ORIGINAL/UPDATED blocks will be ignored.
-Try to keep the patch as small as possible.
+Try to keep the patch as small as possible. Do not use placeholders, as they will make the patch fail.
 
 We can also append to files by prefixing the filename with `append`."""
-
-examples = """
-> User: patch the file `hello.py` to ask for the name of the user
-```patch hello.py
-<<<<<<< ORIGINAL
-    print("Hello world")
-=======
-    name = input("What is your name? ")
-    print(f"hello {name}")
->>>>>>> UPDATED
-```
-
-> User: run the function when the script is run
-```append hello.py
-if __name__ == "__main__":
-    hello()
-```
-""".strip()
 
 ORIGINAL = "<<<<<<< ORIGINAL\n"
 DIVIDER = "\n=======\n"
 UPDATED = "\n>>>>>>> UPDATED"
+
+mode: Literal["markdown", "xml"] = "markdown"
+
+
+def patch_to_markdown(patch: str, filename: str, append: bool = False) -> str:
+    _tool = "patch" if not append else "append"
+    return f"```{_tool} {filename}\n{patch}\n```"
+
+
+def patch_to_xml(patch: str, filename: str, append: bool = False) -> str:
+    _tool = "patch" if not append else "append"
+    return f"<{_tool} filename='{filename}'>\n{patch}\n</patch>"
+
+
+def patch_to_output(patch: str, filename: str, append: bool = False) -> str:
+    if mode == "markdown":
+        return patch_to_markdown(patch, filename, append)
+    elif mode == "xml":
+        return patch_to_xml(patch, filename, append)
+    else:
+        raise ValueError(f"Invalid mode: {mode}")
+
+
+examples = f"""
+> User: patch the file `hello.py` to ask for the name of the user
+> Assistant: {patch_to_output("hello.py", '''
+<<<<<<< ORIGINAL
+    print("Hello world")
+=======
+    name = input("What is your name? ")
+    print(f"Hello {name}")
+>>>>>>> UPDATED
+''')}
+> System: Patch applied
+
+> User: change the codeblock to append to the file
+> Assistant: {patch_to_output("patch.py", '''
+<<<<<<< ORIGINAL
+```save hello.py
+=======
+```append hello.py
+>>>>>>> UPDATED
+''')}
+
+
+> User: run the function when the script is run
+> Assistant: {patch_to_output("hello.py", '''
+<<<<<<< ORIGINAL
+    print("Hello world")
+=======
+    name = input("What is your name? ")
+    print(f"Hello {name}")
+>>>>>>> UPDATED
+''', append=True)}
+""".strip()
 
 
 def apply(codeblock: str, content: str) -> str:
@@ -84,9 +104,29 @@ def apply(codeblock: str, content: str) -> str:
 
     # TODO: maybe allow modified chunk to contain "// ..." to refer to chunks in the original,
     #       and then replace these with the original chunks?
+    re_placeholder = re.compile(r"^[ \t]*(#|//) \.\.\. ?.*$", re.MULTILINE)
+    if re_placeholder.search(original) or re_placeholder.search(modified):
+        # raise ValueError("placeholders in modified chunk")
+        # split them by lines starting with "# ..."
+        originals = re_placeholder.split(original)
+        modifieds = re_placeholder.split(modified)
+        if len(originals) != len(modifieds):
+            raise ValueError(
+                "different number of placeholders in original and modified chunks"
+                f"\n{originals}\n{modifieds}"
+            )
+        new = content
+        for orig, mod in zip(originals, modifieds):
+            if orig == mod:
+                continue
+            new = new.replace(orig, mod)
+    else:
+        if original not in content:  # pragma: no cover
+            raise ValueError("original chunk not found in file", original)
 
-    # replace the original chunk with the modified chunk
-    new = content.replace(original, modified)
+        # replace the original chunk with the modified chunk
+        new = content.replace(original, modified)
+
     if new == content:  # pragma: no cover
         raise ValueError("patch did not change the file")
 
@@ -127,8 +167,8 @@ def execute_patch(
         yield Message("system", "Patch applied")
     except (ValueError, FileNotFoundError) as e:
         yield Message("system", f"Patch failed: {e.args[0]}")
-        
-        
+
+
 tool = ToolSpec(
     name="patch",
     desc="Apply a patch to a file",
@@ -137,3 +177,4 @@ tool = ToolSpec(
     execute=execute_patch,
     block_types=["patch"],
 )
+__doc__ = tool.get_doc(__doc__)
